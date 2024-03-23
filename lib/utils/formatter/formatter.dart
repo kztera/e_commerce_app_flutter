4e358079abdec26d2c139aaf9d6b7e7b59d9b712
kztera <116504460+kztera@users.noreply.github.com>
2024-03-23 12:12:10 +0700

    Add Formatter and PricingCalculator classes

---
+++ lib/utils/formatter/formatter.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+
+class Formatter {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('DD/MM/YYYY').format(date);
+  }
+
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(locale: 'vi_VN', symbol: 'đ').format(amount);
+  }
+
+  static String formatPhoneNumber(String phoneNumber) {
+    // Assuming a 10-digit Vietnamese phone number format: 091 677 5119
+    if (phoneNumber.length == 10) {
+      return '${phoneNumber.substring(0, 3)} ${phoneNumber.substring(3, 6)} ${phoneNumber.substring(6)}';
+    }
+    return phoneNumber;
+  }
+}
