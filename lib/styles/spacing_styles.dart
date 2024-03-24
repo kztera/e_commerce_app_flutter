cbda1dc00cb0770d236fe954b3b97aecc36b49f2
kztera <116504460+kztera@users.noreply.github.com>
2024-03-25 00:38:04 +0700

    Add spacing styles class

---
+++ lib/styles/spacing_styles.dart
@@ -0,0 +1,11 @@
+import 'package:e_commerce_app_flutter_app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class SpacingStyles {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: CustomSizes.appBarHeight,
+    left: CustomSizes.defaultSpace,
+    right: CustomSizes.defaultSpace,
+    bottom: CustomSizes.defaultSpace,
+  );
+}
