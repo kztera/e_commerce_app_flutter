ea011973722520585d6c3a1154190201844a03dd
kztera <116504460+kztera@users.noreply.github.com>
2024-03-24 23:03:10 +0700

    Update splash screen images

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:e_commerce_app_flutter_app/app.dart';
 import 'package:e_commerce_app_flutter_app/i18n/strings.g.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   LocaleSettings.useDeviceLocale();
   runApp(TranslationProvider(child: const App()));
 }
