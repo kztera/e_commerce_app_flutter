878225e320c582bcda5a844428ebd0128bc530ac
kztera <116504460+kztera@users.noreply.github.com>
2024-03-24 22:34:51 +0700

    Add onboarding widget and screen

---
+++ lib/widgets/onboarding/onboarding_dot_navigation.dart
@@ -0,0 +1,28 @@
+import 'package:e_commerce_app_flutter_app/controllers/onboarding_controller.dart';
+import 'package:e_commerce_app_flutter_app/utils/helpers/helper_function.dart';
+import 'package:flutter/material.dart';
+import 'package:e_commerce_app_flutter_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app_flutter_app/utils/devices/device_utility.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:e_commerce_app_flutter_app/utils/constants/colors.dart';
+
+class OnboardingDotNavigation extends StatelessWidget {
+  const OnboardingDotNavigation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunc.isDarkMode(context);
+    final controller = OnboardingController.instance;
+
+    return Positioned(
+        bottom: DeviceUtils.getBottomNavigationBarHeight() + 25,
+        left: CustomSizes.defaultSpace,
+        child: SmoothPageIndicator(
+            count: 3,
+            controller: controller.pageController,
+            onDotClicked: controller.onNavigationPress,
+            effect: ExpandingDotsEffect(activeDotColor: dark ? ThemeColors.light : ThemeColors.dark, dotHeight: 6)));
+  }
+}
