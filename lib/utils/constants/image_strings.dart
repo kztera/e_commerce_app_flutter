c767074b768938730e1e13f07a9316ce97e8e1e2
kztera <116504460+kztera@users.noreply.github.com>
2024-03-24 23:21:18 +0700

    Update onboarding images and remove unused assets

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,8 +1,8 @@
 class Images {
   // -- OnBoarding Texts
-  static const String onBoardingImage1 = "assets/images/onboarding/sammy-line-searching.gif";
-  static const String onBoardingImage2 = "assets/images/onboarding/sammy-line-shopping.gif";
-  static const String onBoardingImage3 = "assets/images/onboarding/sammy-line-delivery.gif";
+  static const String onBoardingImage1 = "assets/images/onboarding/onboarding1.png";
+  static const String onBoardingImage2 = "assets/images/onboarding/onboarding2.png";
+  static const String onBoardingImage3 = "assets/images/onboarding/onboarding3.png";
 
   // -- Social Logos
   static const String google = "assets/logos/google-icon.png";
