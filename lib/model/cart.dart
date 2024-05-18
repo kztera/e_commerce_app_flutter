930bf76ae6cbcec5050b0c6cc0ca27281b05a59b
DrawTok <lth121103@gmail.com>
2024-05-18 14:40:48 +0700

    update model

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -1,28 +1,20 @@
-class Cart {
-  final String id;
-  final String productId;
-  final String productName;
-  final String productImage;
-  final int productPrice;
-  final int productSaleOff;
+import 'package:zzz_book_store/model/item.dart';
 
-  Cart({
-    required this.id,
-    required this.productId,
-    required this.productName,
-    required this.productImage,
-    required this.productPrice,
-    required this.productSaleOff
-  });
+class Cart extends Item {
+  Cart({productId, productName, productImage, productPrice, productSaleOff})
+      : super(
+            id: productId,
+            name: productName,
+            image: productImage,
+            price: productPrice,
+            saleOff: productSaleOff);
 
   factory Cart.fromJson(Map<String, dynamic> json) {
     return Cart(
-      id: json['_id'],
-      productId: json['product'],
-      productName: json['productName'],
-      productImage: json['productImage'],
-      productPrice: json['productPrice'],
-      productSaleOff: json['productSaleOff']
-    );
+        productId: json['product'],
+        productName: json['productName'],
+        productImage: json['productImage'],
+        productPrice: json['productPrice'],
+        productSaleOff: json['productSaleOff']);
   }
 }
--- lib/model/wishlist.dart
+++ lib/model/wishlist.dart
@@ -1,3 +1,30 @@
-class Wishlist {
-  
+import 'package:zzz_book_store/model/item.dart';
+
+class Wishlist extends Item {
+  final bool productExists;
+
+  Wishlist(
+      {productId,
+      productName,
+      productImage,
+      productPrice,
+      productSaleOff,
+      required this.productExists})
+      : super(
+            id: productId,
+            name: productName,
+            image: productImage,
+            price: productPrice,
+            saleOff: productSaleOff);
+
+  factory Wishlist.fromJson(Map<String, dynamic> json) {
+    return Wishlist(
+      productId: json['productId'],
+      productImage: json['productImage'],
+      productPrice: json['productPrice'],
+      productName: json['productName'],
+      productSaleOff: json['productSaleOff'],
+      productExists: json['productExists'],
+    );
+  }
 }
